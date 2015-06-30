8a3086cb3615208daa3ba0ecfbd8257e9fd1f2e5
rombyczk <rombyczk@student.pg.gda.pl>
2015-06-30 23:26:23 +0200

    Sensowny program na podstawie prezentcaji z wielowątkowością

---
+++ Csharp app/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Csharp_app
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            MyInits();
+        }
+
+        void MyInits()
+        {
+             port = 20000;
+             servListener = new ServListener();
+
+             serverThread = new Thread(servListener.serverChecker);
+             serverThread.Start();
+
+         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host = textBox1.Text;
+
+            try
+            {
+                UdpClient client = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(textBox2.Text);
+                client.Send(dane, dane.Length);
+                listBox1.Items.Add(textBox2.Text);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("SEND ERROR CLIENT");
+                MessageBox.Show(ex.ToString(), "CLIENT ERROR");
+            }
+        }
+
+      
+
+
+    };
+
+    class ServListener
+    {
+        int port;
+        UdpClient server;
+        IPEndPoint remoteID;
+        
+        public ServListener()
+        {
+            remoteID = new IPEndPoint(IPAddress.Any, 0);
+            port = 20000;
+        }   
+
+        public void serverChecker()
+        {
+            while (true)
+            {
+                try
+                {
+                    server = new UdpClient(port);
+                    Byte[] received = server.Receive(ref remoteID);
+                    MessageBox.Show(Encoding.ASCII.GetString(received), "NEW MESSAGE");
+                    server.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "SERVER ERROR");
+                }
+            }
+        }
+    };
+}
+
+
+
+
+/**/
